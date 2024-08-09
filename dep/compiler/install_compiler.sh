02e1a458a6c177a5f3317786d1d2fa99c579f353
tharveyy <tom.harvey@capgemini.com>
2024-08-09 13:29:30 +0100

    Re rested build from scratch and fixed minor errors with libc and setting the path for the compiler

--- dep/compiler/install_compiler.sh
+++ dep/compiler/install_compiler.sh
@@ -3,3 +3,4 @@
 curl --output compiler.xz https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/10.2-2020.11/binrel/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf.tar.xz
 tar -xvf compiler.xz
 rm -r compiler.xz
+export PATH=dep/compiler/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf/bin:$PATH
